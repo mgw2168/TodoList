eaa3e75a4d135888c29312900ea769b207dfb26e
mgw2168 <mgw2168@163.com>
2019-08-31 21:28:27 +0800

    first

---
+++ src/Todolist.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <div>
+    <div>
+      <input type="text" v-model="inputValue">
+      <button @click="handleClick">提交</button>
+    </div>
+    <ul>
+      <todo-item v-for="(item, index) of list"
+        :key="index"
+        :content="item"
+        :index="index"
+        @delete="deleteListClick"
+        >
+      </todo-item>
+    </ul>
+  </div>
+</div>
+
+</template>
+
+<script>
+/* eslint-disable */
+import TodoItem from './components/TodoItem'
+export default {
+  // 声明组件
+  components: {
+    'todo-item': TodoItem
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleClick () {
+      if (this.inputValue != '') {
+        this.list.push(this.inputValue)
+        this.inputValue = ''
+      } else {
+        alert("输入内容不能为空！")
+      }
+    },
+    deleteListClick (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li @click="deleteClick">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  methods: {
+    deleteClick () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
